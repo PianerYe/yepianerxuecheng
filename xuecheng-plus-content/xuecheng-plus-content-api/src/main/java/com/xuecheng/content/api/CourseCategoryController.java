32838e9baa58474d38bb7d38c0224b40f898cd80
yepianer <aimeinvshen@126.com>
2023-07-21 07:05:00 +0800

    nacos配置

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseCategoryController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseCategoryController.java
@@ -2,6 +2,8 @@ package com.xuecheng.content.api;
 
 import com.xuecheng.content.model.dto.CourseCategoryTreeDto;
 import com.xuecheng.content.service.CourseCategoryService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +12,14 @@ import javax.annotation.Resource;
 import java.util.List;
 
 @Slf4j
+@Api(value = "课程类别接口",tags = "课程类别接口")
 @RestController
 public class CourseCategoryController {
 
     @Resource
     CourseCategoryService courseCategoryService;
 
+    @ApiOperation("课程类别展示")
     @GetMapping("/course-category/tree-nodes")
     public List<CourseCategoryTreeDto> queryTreeNodes(){
         return courseCategoryService.queryTreeNodes("1");
