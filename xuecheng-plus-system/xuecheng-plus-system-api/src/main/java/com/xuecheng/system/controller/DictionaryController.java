6edebe0c765601953626720ebb1a492ad549e160
yepianer <aimeinvshen@126.com>
2023-07-22 23:20:27 +0800

    修正的swagger开启报类型转换异常，导入了medias项目

--- xuecheng-plus-system/xuecheng-plus-system-api/src/main/java/com/xuecheng/system/controller/DictionaryController.java
+++ xuecheng-plus-system/xuecheng-plus-system-api/src/main/java/com/xuecheng/system/controller/DictionaryController.java
@@ -2,6 +2,8 @@ package com.xuecheng.system.controller;
 
 import com.xuecheng.system.model.po.Dictionary;
 import com.xuecheng.system.service.DictionaryService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +19,7 @@ import java.util.List;
  *
  * @author itcast
  */
+@Api(value = "数据字典管理接口",tags = "数据字典管理管理接口")
 @Slf4j
 @RestController
 public class DictionaryController  {
@@ -24,11 +27,13 @@ public class DictionaryController  {
     @Autowired
     private DictionaryService  dictionaryService;
 
+    @ApiOperation("数据字典的查询接口")
     @GetMapping("/dictionary/all")
     public List<Dictionary> queryAll() {
         return dictionaryService.queryAll();
     }
 
+    @ApiOperation("根据代码查询数据字典的对应类型接口")
     @GetMapping("/dictionary/code/{code}")
     public Dictionary getByCode(@PathVariable String code) {
         return dictionaryService.getByCode(code);
