0b4a2fb723d836a25199122f9e3bab537828c344
yepianer <aimeinvshen@126.com>
2023-10-20 00:25:25 +0800

    授权-从数据库查询权限

--- xuecheng-plus-auth/src/main/java/com/xuecheng/ucenter/service/impl/UserServiceImpl.java
+++ xuecheng-plus-auth/src/main/java/com/xuecheng/ucenter/service/impl/UserServiceImpl.java
@@ -2,9 +2,11 @@ package com.xuecheng.ucenter.service.impl;
 
 import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.xuecheng.ucenter.mapper.XcMenuMapper;
 import com.xuecheng.ucenter.mapper.XcUserMapper;
 import com.xuecheng.ucenter.model.dto.AuthParamsDto;
 import com.xuecheng.ucenter.model.dto.XcUserExt;
+import com.xuecheng.ucenter.model.po.XcMenu;
 import com.xuecheng.ucenter.model.po.XcUser;
 import com.xuecheng.ucenter.service.AuthService;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +18,8 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author yepianer
@@ -29,6 +33,8 @@ public class UserServiceImpl implements UserDetailsService {
 
     @Resource
     XcUserMapper xcUserMapper;
+    @Resource
+    XcMenuMapper xcMenuMapper;
 
     @Resource
     ApplicationContext applicationContext;
@@ -54,6 +60,7 @@ public class UserServiceImpl implements UserDetailsService {
         //调用统一execute方法完成认证
         XcUserExt xcUserExt = authService.execute(authParamsDto);
         //封装XcUserExt用户信息为Use'r'Details
+        //根据UserDetails对象生成令牌
         UserDetails userPrincipal = getUserPrincipal(xcUserExt);
 
         return userPrincipal;
@@ -70,6 +77,18 @@ public class UserServiceImpl implements UserDetailsService {
         String password = xcUser.getPassword();
         //权限
         String[] authorities = {"test"};
+        //根据用户的id查询用户的权限
+        List<XcMenu> xcMenus = xcMenuMapper.selectPermissionByUserId(xcUser.getId());
+        if (xcMenus.size()>0){
+            List<String> permissions = new ArrayList<>();
+            xcMenus.forEach(m->{
+                //拿到用户拥有的权限标识符
+                permissions.add(m.getCode());
+            });
+            //将permissions转成数组
+            authorities = permissions.toArray(new String[0]);
+        }
+
         xcUser.setPassword(null);
         //将用户的信息转成json
         String userJson = JSON.toJSONString(xcUser);
